1062822c881bf4285bdeb4ddc89f94b8d3986ed6
linda <yangyus@uci.edu>
2019-03-18 22:52:43 -0700

    update

--- project1/logs/parseTSTJ.py
+++ project1/logs/parseTSTJ.py
@@ -10,8 +10,8 @@ for filename in glob.glob('log*.txt'):
 
     for i in file.readlines():
         list_ts_tj=i.split()
-        ts+=int(list_ts_tj[0])
-        tj += int(list_ts_tj[1])
+        tj+=int(list_ts_tj[0])
+        ts += int(list_ts_tj[1])
         total+=1
 
     avg_ts=ts/total/1000000
