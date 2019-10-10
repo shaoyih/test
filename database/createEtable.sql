152b68fd23312c841496fb0dcca2cc2d3360b365
shaoyih <shaoyih2@gmail.com>
2019-10-09 23:53:29 -0700

    managed dir

--- database/createEtable.sql
+++
@@ -1,8 +0,0 @@
-USE moviedb;
-drop table if exists employees;
-CREATE TABLE employees(
-	email varchar(50) primary key,
-	password varchar(20) not null,
-	fullname varchar(100)
-);
-insert INTO employees VALUES('classta@email.edu', 'classta','TA CS122B');
